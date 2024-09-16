c16e3c787d641301eb3a57206fdcce0ab52d97e5
bruno duarte <159266616+Brunoduartemarcanzoni@users.noreply.github.com>
2024-09-16 09:24:43 -0300

    Add files via upload
    16/05

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function calcular(){
     const c= parseFloat(document.getElementById('val_c').value);
     let x1;
     let x2;
-    const delta= (b * b * -(4 * a * c));
+    const delta= (b * b ) -(4 * a * c);
     const raiz_delta= Math.sqrt(delta);
 
     if(delta<0){
-        document.getElementById('res_1').innerText="Não há raiz reias"
+        document.getElementById('res_1').innerText="Não há raiz reias";
     }else if (delta>0){
         x1= (-b*raiz_delta)/(2*a);
         x2= (-b*raiz_delta)/(2*a);
